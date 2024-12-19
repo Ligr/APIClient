2a61931303bd87f64bc0c18f533f9fddc7a18167
Alex <aliaksandr.huryn@gmail.com>
2024-12-19 17:01:43 +0300

    Add public API

--- Sources/APIClient/APIClient.swift
+++ Sources/APIClient/APIClient.swift
@@ -2,9 +2,10 @@ import Foundation
 
 public protocol APIClient: Sendable {
     func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint) async throws
-    func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable
     func request<Endpoint: APIEndpoint, DataType: Encodable>(_ endpoint: Endpoint, data: DataType) async throws
+    func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable
     func request<Endpoint: APIEndpoint, DataType: Encodable>(_ endpoint: Endpoint, data: DataType) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable
+    func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint, form: [String: String]) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable
 }
 
 public struct APIClientImpl: APIClient {
@@ -24,34 +25,34 @@ public struct APIClientImpl: APIClient {
         _ = try await httpClient.execute(request)
     }
 
-    public func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable {
-        let request = endpoint.request
-        let data = try await httpClient.execute(request)
-        do {
-            return try jsonDecoder.decode(Endpoint.ResultType.self, from: data.0)
-        } catch {
-            print("❌ failed to decode model '\(Endpoint.ResultType.self)' error: \(error), data: \(String(data: data.0, encoding: .utf8) ?? "nil")")
-            throw error
-        }
-    }
-
     public func request<Endpoint: APIEndpoint, DataType: Encodable>(_ endpoint: Endpoint, data: DataType) async throws {
         let request = endpoint.request(json: data, encoder: jsonEncoder)
         _ = try await httpClient.execute(request)
     }
 
-    public func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint, form: [String: String]) async throws {
+    public func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable {
+        let request = endpoint.request
+        return try await requestDecodable(request)
+    }
+
+    public func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint, form: [String: String]) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable {
         let request = endpoint.request(form: form)
-        _ = try await httpClient.execute(request)
+        return try await requestDecodable(request)
     }
 
     public func request<Endpoint: APIEndpoint, DataType: Encodable>(_ endpoint: Endpoint, data: DataType) async throws -> Endpoint.ResultType where Endpoint.ResultType: Decodable {
         let request = endpoint.request(json: data, encoder: jsonEncoder)
+        return try await requestDecodable(request)
+    }
+
+    // MARK: - Helpers
+
+    private func requestDecodable<ResultType: Decodable>(_ request: URLRequest) async throws -> ResultType {
         let data = try await httpClient.execute(request)
         do {
-            return try jsonDecoder.decode(Endpoint.ResultType.self, from: data.0)
+            return try jsonDecoder.decode(ResultType.self, from: data.0)
         } catch {
-            print("❌ failed to decode model '\(Endpoint.ResultType.self)' error: \(error), data: \(String(data: data.0, encoding: .utf8) ?? "nil")")
+            print("❌ failed to decode model '\(ResultType.self)' error: \(error), data: \(String(data: data.0, encoding: .utf8) ?? "nil")")
             throw error
         }
     }
