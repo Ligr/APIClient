f77aa1d2a06b63105420735da13ace87c8fe48c7
Alex <aliaksandr.huryn@gmail.com>
2025-01-31 11:14:58 +0300

    Fix Linux build

--- Sources/APIClient/APIClient.swift
+++ Sources/APIClient/APIClient.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public protocol APIClient: Sendable {
     func request<Endpoint: APIEndpoint>(_ endpoint: Endpoint) async throws
